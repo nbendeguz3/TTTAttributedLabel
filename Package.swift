405460de6aed376577251e4e69f4850a453a7479
Bendeguz Nemeth <n.bendeguz3@gmail.com>
2020-09-08 09:29:00 +0200

    Added SwiftPM support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "TTTAttributedLabel",
+    platforms: [.iOS(.v8)],
+    products: [
+        .library(
+            name: "TTTAttributedLabel",
+            targets: ["TTTAttributedLabel"]
+        )
+    ],
+    targets: [
+        .target(name: "TTTAttributedLabel",
+                path: "TTTAttributedLabel")
+    ]
+)
